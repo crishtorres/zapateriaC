9905c503adccd43f78a29b9a4d59694f9290530f
Cristian Torres <cristian.torres.9035@gmail.com>
2017-09-22 17:07:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -17,47 +17,67 @@ int main(){
     char nCompleto[50];
     int diaNac = 0;
     int prod = 0;
-    int ret = 0;
+    int ret = 0; 
+    int ret1 = 0;
     int marca = 0;
     float precio = 0;
     int cantidad = 0;
     int i=0;
     float talle=0;
-    
+    int nroPedido = 0;
+    float impRecibido=0;
+    float total =0 ;
+    float descuentos =0;
+    float vuelto =0;
+
     do{       
         printf("¿Desea comenzar a atender? Ingrese SI o NO\n");
         scanf("%s",respuesta);    
-        if(respuesta!="SI" && respuesta!="NO"){
-            printf("Respuesta incorrecta.Solo se admite SI o NO. Presione enter para continuar\n");   
+
+        ret = strncmp("SI",respuesta,2);
+        ret1 = strncmp("NO",respuesta,2);             
+
+        if(ret!=0 && ret1!=0){
+            printf("Respuesta incorrecta. Solo se admite SI o NO. Intente Nuevamente.\n \n");   
         }
-    }while(respuesta!="SI" && respuesta!="NO");
-    
-    ret = strcmp("SI",respuesta);
+
+    }while(ret==0 && ret1==0);
+
+
+
+    ret = strncmp("SI",respuesta,2);
 
     while(ret==0){
 
+    	nroPedido = 0;
         system("clear");
-        printf("Ingrese Nombre del cliente \n");
+        printf("Ingrese Nombre del cliente :\n");
         scanf("%s",nombre);
         
-        system("clear");
-        printf("Ingrese Apellido del cliente \n");
+        
+        printf("\nIngrese Apellido del cliente :\n");
         scanf("%s",apellido);
            
-        nCompleto = strcat(nombre,apellido);
-        
-        system("clear");
-        printf("Ingrese dia de nacimiento \n");
+        strcat(nombre," ");
+        strcat(nombre,apellido);
+
+        printf("\nIngrese dia de nacimiento :\n");
         scanf("%i",&diaNac);
 
+        system("clear");
+
         do{
+        	nroPedido++;
             system("clear");
+            printf("Pedido Nº %d de %s \n \n",nroPedido,nombre);
+
             printf("Seleccione el Producto a comprar \n1.Zapatillas \n2.Botines \n3.Zapatos\n");
             scanf("%d",&prod);
 
             system("clear");
+            printf("Pedido Nº %d de %s \n \n",nroPedido,nombre);
             switch(prod){
-                case 1:
+                case 1:      
                     printf("Seleccione Marca de la Zapatilla \n1.Nike \n2.Adadis \n3.New Balance\n");
                     scanf("%d",&marca);
                     switch(marca){
@@ -87,22 +107,25 @@ int main(){
             }
 
             system("clear");
-            
+            printf("Pedido Nº %d de %s \n \n",nroPedido,nombre);
+
             do{
                 printf("Ingrese la cantidad de pares a adquirir\n");
                 scanf("%d",&cantidad);
                 
                 if(cantidad<=0){
-                    printf("La cantidad ingresada es incorrecta! Presione enter e intente nuevamente\n");
+                    printf("La cantidad ingresada es incorrecta! Intente nuevamente. \n");
                 }
-            }while (cantidad!=0);
+            }while (cantidad<0);
                 
             //printf("Ingrese la cantidad de pares a adquirir\n");
             //scanf("%d",&cantidad);
 
             system("clear");
+            //printf("Pedido Nº %d de %s \n \n",nroPedido,nombre);
             for(i=0;i<cantidad;i++){
-                printf("ingrese el talle para el par nº %d \n",i);
+                printf("Pedido Nº %d de %s \n \n",nroPedido,nombre);
+                printf("ingrese el talle para el par nº %d \n",i+1);
                 scanf("%f",&talle);
 
                 if(talle>44){
@@ -111,18 +134,61 @@ int main(){
                 system("clear");
             }
 
-            printf("Precio final %f \n",precio);
+            do{       
+		        printf("¿Desea seguir comprando para el cliente %s? Ingrese SI o NO\n",nombre);
+		        scanf("%s",respuesta);    
 
-            //system("clear");
-            printf("¿Desea seguir comprando? Ingrese SI o NO\n");
-            scanf("%s",respuesta);
-            ret = strcmp("SI",respuesta);
+		        ret = strncmp("SI",respuesta,2);
+		        ret1 = strncmp("NO",respuesta,2);             
 
+		        if(ret!=0 && ret1!=0){
+		            printf("Respuesta incorrecta. Solo se admite SI o NO. Intente Nuevamente.\n \n");   
+		        }
+
+		    }while(ret==0 && ret1==0);
+
+            ret = strncmp("SI",respuesta,2);
+            // SI == 0
+            // NO != 0
         }while(ret==0);
+        //SI PONE QUE NO DESEA SEGUIR ATENDIENDO A EL CLIENTE ACTUAL MUESTRO LOS TOTALES        
 
-        system("clear");
-        printf("¿Desea seguir atendiendo? Ingrese SI o NO\n");
-        scanf("%s",respuesta);
+        printf("*********************************************************************\n");
+        printf("\n");
+        printf(" PEDIDO DE %s \n", nombre);
+        printf("\n");
+        printf("IMPORTE ****************************************************** %-3.2f \n",precio);
+        printf("\n");
+        printf("DESCUENTOS *************************************************** %-3.2f \n",descuentos);
+        printf("\n");
+        total = precio - descuentos;
+        printf("TOTAL ******************************************************** %-3.2f \n",total); 
+        printf("\n");
+        printf("*********************************************************************\n");
+
+        printf("Ingrese importe recibido \n");
+        scanf("%f",&impRecibido);
+
+        vuelto = (impRecibido - total);
+
+        printf("El vuelto es %-3.2f \n",vuelto);
+
+        //system("clear");
+        
+        do{       
+	        printf("¿Desea seguir atendiendo? Ingrese SI o NO\n");
+	        scanf("%s",respuesta);    
+
+	        ret = strncmp("SI",respuesta,2);
+	        ret1 = strncmp("NO",respuesta,2);             
+
+	        if(ret!=0 && ret1!=0){
+	            printf("Respuesta incorrecta. Solo se admite SI o NO. Intente Nuevamente.\n \n");   
+	        }
+
+		}while(ret==0 && ret1==0);
+
+        ret = strncmp("SI",respuesta,2);
 
     }
 
